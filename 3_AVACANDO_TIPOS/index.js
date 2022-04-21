6288b0c9387fd980f370363673b9f1b4f5783508
BrunaDuarte-3321 <62619506+BrunaDuarte-3321@users.noreply.github.com>
2022-04-21 08:03:07 -0300

    Finalizando modulo de tipos avançados TS.

--- 3_AVACANDO_TIPOS/index.js
+++ 3_AVACANDO_TIPOS/index.js
@@ -27,4 +27,81 @@ setTimeout(() => {
     const sallary = 1000;
     //console.log(sallary)
 }, 2000);
-// 7 - 
+// 7 - tipos de objeto
+const passCoordiantes = (cood) => {
+    console.log('X coordinates: ' + cood.x);
+    console.log('Y coordinates: ' + cood.y);
+};
+const objCoord = { x: 329, y: 84.2 };
+passCoordiantes(objCoord);
+// 8 - porps opcionais
+const showNumbers = (a, b, c) => {
+    console.log(`A: ${a}`);
+    console.log(`B: ${b}`);
+    console.log(`C: ${c}`);
+};
+showNumbers(2, 2);
+// 9 - validação de props opcionais
+const advancedGreeting = (firstName, lastName) => {
+    if (lastName !== undefined) {
+        return `Olá, ${firstName} ${lastName}, tudo bem?`;
+    }
+    return `Olá, ${firstName}, tudo bem?`;
+};
+console.log(advancedGreeting('Bruna', 'Duarte'));
+console.log(advancedGreeting('Bruna'));
+// 10 Union types
+const showBalance = (balance) => {
+    console.log(`O saldo da conta é R$ ${balance}`);
+};
+showBalance('500');
+showBalance(100);
+const array2 = [1, 'teste', true];
+console.log(array2);
+// 11 - Avançando com Union types
+const showUserRole = (role) => {
+    if (typeof role === 'boolean') {
+        return 'Usuário não aprovado';
+    }
+    return `A função do usuário é: ${role}`;
+};
+console.log(showUserRole(false));
+console.log(showUserRole('Admin'));
+const showId = (id) => {
+    console.log(`O Id é: ${id}`);
+};
+showId(1);
+showId('200');
+const showCoordes = (obj) => {
+    console.log(`X: ${obj.x} Y: ${obj.y} Z: ${obj.z}`);
+};
+const coordObj = {
+    x: 10,
+    y: 15,
+    z: 20,
+};
+showCoordes(coordObj);
+const somePerson = { name: 'Bruna', age: 28 };
+console.log(somePerson);
+// type alias não pode ser implementada.
+// 15 - Literal types
+let test;
+test = 'testando';
+console.log(test);
+const showDiction = (direction) => {
+    console.log(`A direção é: ${direction}`);
+};
+showDiction('center'); // só aceita os valores tipados no paramentro
+// 16 - Non-null Assertion Operation
+const p = document.getElementById('some-p');
+console.log(p.innerText); // quando tem um elemento do dom e vc precisa identificar no ts para fazer manipulação
+// 17 Bigint
+let n;
+//n = 1
+n = 1000n; // só funciona com versão 2020 do ts
+console.log(n);
+// 18 Symbol
+let simbolA = Symbol('A');
+let simbolB = Symbol('A');
+console.log(simbolA === simbolB);
+console.log(simbolA == simbolB);
--- 3_AVACANDO_TIPOS/index.ts
+++ 3_AVACANDO_TIPOS/index.ts
@@ -49,4 +49,138 @@ setTimeout(() => {
   //console.log(sallary)
 },2000)
 
-// 7 - 
+// 7 - tipos de objeto
+
+const passCoordiantes = (cood:{x:number, y:number}) =>{
+  console.log( 'X coordinates: ' + cood.x)
+  console.log( 'Y coordinates: ' + cood.y)
+}
+
+const objCoord = {x:329, y:84.2}
+
+passCoordiantes(objCoord)
+
+// 8 - porps opcionais
+
+const showNumbers = (a:number, b:number, c?:number) => {
+  console.log(`A: ${a}`)
+  console.log(`B: ${b}`)
+  console.log(`C: ${c}`)
+}
+showNumbers(2,2)
+
+// 9 - validação de props opcionais
+
+const advancedGreeting = ( firstName: string, lastName?: string) =>{
+
+    if(lastName !== undefined){
+      return  `Olá, ${firstName} ${lastName}, tudo bem?` 
+    }
+    return `Olá, ${firstName}, tudo bem?`
+}
+
+console.log(advancedGreeting('Bruna', 'Duarte') )
+console.log(advancedGreeting('Bruna' ) )
+
+// 10 Union types
+
+const showBalance = (balance: string | number) =>{
+  console.log(`O saldo da conta é R$ ${balance}`)
+}
+
+showBalance( '500')
+showBalance( 100)
+
+const array2: Array<number | string | boolean> = [1, 'teste', true]
+
+console.log(array2)
+
+// 11 - Avançando com Union types
+
+const showUserRole = (role:boolean | string) =>{
+  if(typeof role === 'boolean'){
+    return 'Usuário não aprovado'
+  }
+  return `A função do usuário é: ${role}`
+}
+console.log(showUserRole(false))
+console.log(showUserRole('Admin'))
+
+// 12 - types alias
+type ID = string | number
+const showId = (id: ID) =>{
+  console.log(`O Id é: ${id}`)
+}
+
+showId(1)
+showId('200')
+
+// 13 - Introdução às interfaces
+
+interface Point {
+  x: number;
+  y: number;
+  z:number;
+}
+
+const showCoordes = (obj: Point) =>{
+console.log(`X: ${obj.x} Y: ${obj.y} Z: ${obj.z}`)
+}
+
+const coordObj:Point = {
+  x:10,
+  y:15,
+  z:20,
+}
+
+showCoordes(coordObj)
+
+// 14 - Diferença entre type alias e interface
+
+interface Person {
+  name:string
+}
+interface Person{
+  age:number
+}
+
+const somePerson: Person ={name: 'Bruna', age:28}
+console.log(somePerson)
+// type alias não pode ser implementada.
+
+// 15 - Literal types
+
+let test: 'testando'
+
+test = 'testando'
+console.log(test)
+
+const showDiction = (direction: 'left' | 'right'| 'center') =>{
+  console.log(`A direção é: ${direction}`)
+}
+
+showDiction('center') // só aceita os valores tipados no paramentro
+
+// 16 - Non-null Assertion Operation
+
+const p = document.getElementById('some-p')
+
+console.log(p!.innerText) // quando tem um elemento do dom e vc precisa identificar no ts para fazer manipulação
+
+// 17 Bigint
+
+let n: bigint
+
+//n = 1
+
+n = 1000n // só funciona com versão 2020 do ts
+
+console.log( n)
+
+// 18 Symbol
+
+ let simbolA:symbol = Symbol('A')
+ let simbolB:symbol = Symbol('A')
+
+ console.log(simbolA === simbolB)
+ console.log(simbolA == simbolB)
