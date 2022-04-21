ef48c4d76d42da254edcf6cda3c796a143cafc94
BrunaDuarte-3321 <62619506+BrunaDuarte-3321@users.noreply.github.com>
2022-04-21 07:10:38 -0300

    primeiro git do curso de typescrip

---
+++ index.js
@@ -0,0 +1,29 @@
+"use strict";
+// 1 - arrays
+const numbers = [1, 2, 3, 5];
+numbers.push(7);
+console.log(numbers[2]);
+const names = ['Bruna', "Duarte"];
+names.push('ola');
+console.log(names);
+// 2 - Outra sintaxe array
+const nums = [200, 300, 422];
+// 3 - any
+const arr1 = [1, 'ola', [], { nome: 'Bruna' }];
+arr1.push(2);
+console.log(arr1);
+// 4 - paramentros tipados
+const soma = (a, b) => {
+    console.log(a + b);
+};
+soma(4, 5);
+// 5 - retorno de função
+const greeting = (name) => {
+    return `Olá ${name}`;
+};
+console.log(greeting('Bruna'));
+// 6 - função anonimas
+setTimeout(() => {
+    const sallary = 1000;
+    //console.log(sallary)
+}, 2000);
---
+++ index.ts
@@ -0,0 +1,50 @@
+// 1 - arrays
+
+const numbers: number[] = [1,2,3,5]
+
+numbers.push(7)
+
+console.log(numbers[2])
+
+
+const names:string[] = ['Bruna', "Duarte"]
+
+names.push('ola')
+
+console.log(names)
+
+// 2 - Outra sintaxe array
+
+const nums:Array<number> = [200,300,422]
+
+// 3 - any
+
+const arr1:any = [1,'ola',[],{nome: 'Bruna'}]
+
+arr1.push(2)
+console.log(arr1)
+
+// 4 - paramentros tipados
+
+const soma = (a:number, b:number) =>{
+  console.log(a+b)
+}
+
+soma(4,5)
+
+// 5 - retorno de função
+
+const greeting = (name: string): string =>{
+  return `Olá ${name}`
+}
+
+
+console.log(greeting('Bruna') )
+
+// 6 - função anonimas
+
+setTimeout(() => {
+  const sallary:number = 1000
+
+  //console.log(sallary)
+},2000)
